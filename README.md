ad45383323caabd042508993052c4560ee8ca3cf
JacksonBarker <jackson@jacksonbarker.ca>
2021-08-19 13:30:49 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Dog-Watchers
-Basic WebSocket server (Python) and client (HTML) for users to track who is watching tho dog.
+# Dog Watchers
+Basic WebSocket server (Python) and client (HTML) for users to track who is watching the dog. (Mobile friendly)
+
+To start the server, run this command (requires pipenv):
+```
+pipenv run python dogwatchers.py
+```
+or, run:
+```
+pip install websockets
+
+python3 dogwatchers.py
+```
+
+![Preview](readme/preview.png)
---
+++ dogwatchers.py
@@ -0,0 +1,54 @@
+import asyncio
+import json
+import logging
+import websockets
+from datetime import datetime
+
+logging.basicConfig()
+
+STATE = {"watcher":"?"}
+
+
+USERS = set()
+
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+async def swap(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            STATE["watcher"] = data["action"] + " started watching at: " + datetime.now().strftime("%H:%M")
+            await notify_state()
+            print(STATE["watcher"])
+    finally:
+        await unregister(websocket)
+
+start_server = websockets.serve(swap, "127.0.0.1", 6789)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
